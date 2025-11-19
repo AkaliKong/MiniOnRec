fbfe8030a1392656ba004823564f397c343c534a
Xiaoyu Kong <44406991+AkaliKong@users.noreply.github.com>
2025-11-19 16:20:39 +0800

    Update amazon_text2emb.sh

--- rq/text2emb/amazon_text2emb.sh
+++ rq/text2emb/amazon_text2emb.sh
@@ -1,5 +1,4 @@
-python amazon_text2emb.py \
-      --dataset Industrial_and_Scientific \
-      --root ../../data/Amazon/index \
-      --plm_name qwen \
-      --plm_checkpoint your_emb_model_path
+accelerate launch --num_processes 8 amazon_text2emb.py \
+    --dataset Industrial_and_Scientific \
+    --root ../../data/Amazon18/Industrial_and_Scientific \
+    --plm_checkpoint your_emb_model_path
